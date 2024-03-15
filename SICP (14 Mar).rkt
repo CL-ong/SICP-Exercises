4dc8b311ab7c5f3d120a7cc370235d75a16861c6
CL-ong <121706384+CL-ong@users.noreply.github.com>
2024-03-16 00:05:33 +0800

    Add files via upload

--- SICP (14 Mar).rkt
+++ SICP (14 Mar).rkt
@@ -15,3 +15,20 @@
 
 ;Ex 1.17
 
+;qn specifies "log number of steps" which indicates we need to repeatedly divide using the same integer
+
+;recursive procedure
+
+(define (fast-mul a b)
+  (cond ((= b 0) 0)
+        ((= b even) (* (double a) (fast-mul a (halve b))))
+        ((= b odd) (+ b (fast-mul a (- b 1))))))
+;translate above proc. into iterative
+
+(define (fast-mul a b)
+  (define (mul-iter result a b)
+    (cond ((= b 0) result)
+          ((even? b) (mul-iter result (double a) (halve b)))
+          (else (mul-iter (* a b) (- b 1)))))
+  (mul-iter 0 a b))
+        
